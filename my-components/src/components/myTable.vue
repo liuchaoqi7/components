d053173b62d43357cd8a51aabd184d0017888985
liuchaoqi1.1 <765922449@qq.com>
2020-11-27 09:50:58 +0800

    table表格组件封装

---
+++ my-components/src/components/myTable.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="wraper">
+    <h1>自己封装的element组件</h1>
+    <h3>table表格组件</h3>
+    <el-table :data="tableDatas.tableData" style="width: 100%">
+      <el-table-column type="selection" v-if="tableDatas.selectionShow">
+      </el-table-column>
+      <el-table-column
+        type="index"
+        :label="tableDatas.indexLabel"
+        v-if="tableDatas.indexShow"
+      >
+      </el-table-column>
+      <el-table-column
+        v-for="(item, index) in tableDatas.labelProp"
+        :key="index"
+        :prop="item.prop"
+        :label="item.label"
+      >
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    tableDatas: {
+      type: Object,
+      default: () => {
+        return {
+          selectionShow: true,
+          indexShow: true,
+          indexLabel: "序号",
+          labelProp: [
+            { label: "姓名", prop: "name" },
+            { label: "地址", prop: "address" },
+            { label: "日期", prop: "date" },
+          ],
+          tableData: [
+            {
+              date: "2016-05-02",
+              name: "王小虎",
+              address: "上海市普陀区金沙江路 1518 弄",
+            },
+            {
+              date: "2016-05-04",
+              name: "王小虎",
+              address: "上海市普陀区金沙江路 1517 弄",
+            },
+            {
+              date: "2016-05-01",
+              name: "王小虎",
+              address: "上海市普陀区金沙江路 1519 弄",
+            },
+            {
+              date: "2016-05-03",
+              name: "王小虎",
+              address: "上海市普陀区金沙江路 1516 弄",
+            },
+          ],
+        };
+      },
+    },
+  },
+  data() {
+    return {
+      // tableData: [
+      //   {
+      //     date: "2016-05-02",
+      //     name: "王小虎",
+      //     address: "上海市普陀区金沙江路 1518 弄",
+      //   },
+      //   {
+      //     date: "2016-05-04",
+      //     name: "王小虎",
+      //     address: "上海市普陀区金沙江路 1517 弄",
+      //   },
+      //   {
+      //     date: "2016-05-01",
+      //     name: "王小虎",
+      //     address: "上海市普陀区金沙江路 1519 弄",
+      //   },
+      //   {
+      //     date: "2016-05-03",
+      //     name: "王小虎",
+      //     address: "上海市普陀区金沙江路 1516 弄",
+      //   },
+      // ],
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ my-components/src/views/home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="wraper">
+    <my-table
+      :tableDatas="{
+        labelProp: [
+          { label: '城市', prop: 'city' },
+          { label: '县名', prop: 'xianming' },
+          { label: '乡镇', prop: 'xiangzhen' },
+        ],
+        fo,
+      }"
+    ></my-table>
+  </div>
+</template>
+
+<script>
+import myTable from "@/components/myTable";
+export default {
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App",
+    };
+  },
+  components: {
+    myTable,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
